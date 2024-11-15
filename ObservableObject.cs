98ef4695001eedba0542c939f9197f4ff34cf4ea
Jonathan2 <Jonathan2@JONATHAN2-PC>
2024-11-15 08:57:42 -0700

    Version 1.1 - simplified, improved performance

--- ObservableObject.cs
+++ ObservableObject.cs
@@ -1,8 +1,8 @@
 /* ObservableObject.cs
- * @version: 1.0
+ * @version: 1.1
  * @author: Jonathan Hilgeman <jhilgeman@gmail.com>
  * @created: 2020-03-04
- * @updated: 2023-04-03
+ * @updated: 2024-11-15
  */
 using System;
 using System.Collections.Generic;
@@ -15,76 +15,54 @@ namespace BaseClasses
     [Serializable]
     public class ObservableObject : INotifyPropertyChanged
     {
-        [XmlIgnore]
-        public Dictionary<string, object> __BackingFields = new Dictionary<string, object>();
-        [XmlIgnore]
-        public Dictionary<string, PropertyInfo> __PropertyInfos = new Dictionary<string, PropertyInfo>();
-
         #region INotifyPropertyChanged Members
         public event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        public void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null, params string[] addlNotify)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            foreach (string alsoNotify in addlNotify)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(alsoNotify));
+            }
         }
         #endregion
 
+        // Where all the values are truly stored
+        [XmlIgnore]
+        private Dictionary<string, object> __Values = new Dictionary<string, object>();
+
         public T Get<T>([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
         {
-            return (T)GetValue(propertyName);
+            if (!__Values.ContainsKey(propertyName))
+            {
+                // Trying to get a value that isn't set yet
+                return default(T);
+            }
+
+            // Value exists, cast it to our desired type and return it
+            return (T)__Values[propertyName];
         }
 
-        public bool Set(object value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null, params string[] addlNotify)
+        public bool Set(object value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
         {
-            if (SetValue(propertyName, value))
+            if (!__Values.ContainsKey(propertyName))
             {
+                // Value isn't set yet
+                __Values.Add(propertyName, value);
                 NotifyPropertyChanged(propertyName);
-                foreach (string alsoNotify in addlNotify)
-                {
-                    NotifyPropertyChanged(alsoNotify);
-                }
                 return true;
             }
-            return false;
-        }
 
-        private object GetValue(string propertyName)
-        {
-            if (!__BackingFields.ContainsKey(propertyName))
+            if (value != __Values[propertyName])
             {
-                var propertyInfo = GetPropertyInfo(propertyName);
-                object defaultValue = propertyInfo.PropertyType.IsValueType ? Activator.CreateInstance(propertyInfo.PropertyType) : null;
-                __BackingFields.Add(propertyName, defaultValue);
-            }
-            return __BackingFields[propertyName];
-        }
-
-        private bool SetValue(string propertyName, object value)
-        {
-            if (!__BackingFields.ContainsKey(propertyName))
-            {
-                __BackingFields.Add(propertyName, value);
+                // Value exists and is different
+                __Values[propertyName] = value;
+                NotifyPropertyChanged(propertyName);
                 return true;
             }
-            else
-            {
-                if (value != __BackingFields[propertyName])
-                {
-                    __BackingFields[propertyName] = value;
-                    return true;
-                }
-            }
-            return false;
-        }
 
-        private PropertyInfo GetPropertyInfo(string propertyName)
-        {
-            if (!__PropertyInfos.ContainsKey(propertyName))
-            {
-                var pi = this.GetType().GetProperty(propertyName);
-                if (pi == null) { throw new System.Exception("Property " + propertyName + " not found!"); }
-                __PropertyInfos.Add(propertyName, pi);
-            }
-            return __PropertyInfos[propertyName];
+            // Value exists but isn't different
+            return false;
         }
 
     }
